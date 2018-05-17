b90a6bbb26e02eaf8f08976279db3733207796a9
lzlss <1187704647@qq.com>
2018-05-17 09:09:08 +0800

    18-5-17 lu

---
+++ shoppingmall-api/src/main/java/com/zhkj/vo/specificationRelation_vo/SpecificationRelationVO.java
@@ -0,0 +1,13 @@
+package com.zhkj.vo.specificationRelation_vo;
+
+public class SpecificationRelationVO {
+    private Integer commodityId;
+
+    public Integer getCommodityId() {
+        return commodityId;
+    }
+
+    public void setCommodityId(Integer commodityId) {
+        this.commodityId = commodityId;
+    }
+}
