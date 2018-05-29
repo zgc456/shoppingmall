ff2f733958dd39be6797a10fcd75f2dd06bbdb48
lzlss <1187704647@qq.com>
2018-05-29 19:07:30 +0800

    18-5-29 lu

--- shoppingmall-dao/src/main/java/com/zhkj/mapper/mycollect_mapper/MyCollectMapper.java
+++ shoppingmall-dao/src/main/java/com/zhkj/mapper/mycollect_mapper/MyCollectMapper.java
@@ -5,6 +5,10 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Map;
+
+/**
+ * 我的收藏
+ */
 @Repository
 public interface MyCollectMapper {
     /**
@@ -14,7 +18,17 @@ public interface MyCollectMapper {
      */
     List<MycollectEntity> queryByUserIdCollectAll(Map<String,Object> map);
 
+    /**
+     * 添加我的收藏
+     * @param map
+     * @return
+     */
     int save(Map<String,Object> map);
 
+    /**
+     * 删除我的收藏
+     * @param map
+     * @return
+     */
     int deleteCollectById(Map<String,Object> map);
 }
